81bf369f679bceb55fec580818dbb1c918ac01cf
mariavsl518 <mariavsl518@gmail.com>
2023-10-06 11:37:01 +0200

    done1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('afterbegin', markup(galleryItems))
+gallery.addEventListener('click', handlerClick)
+
+
+function markup(arr) { 
+    return arr.map(({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+    `).join('');
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+function handlerClick(event) {
+    event.preventDefault();
+}
 
 console.log(galleryItems);
+
