0bc3071e5e7a2e2b20e7de625dce99575e8899a5
mariavsl518 <mariavsl518@gmail.com>
2023-10-07 19:31:27 +0200

    done2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
 
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+var throttle = require('lodash.throttle');
+
+const getTime = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+    console.log(data);
+};
+
+player.on('timeupdate', throttle(getTime, 600))
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
