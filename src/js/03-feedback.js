120939131591ed7d9eaa8be8bf5b0e5483f2ba66
mariavsl518 <mariavsl518@gmail.com>
2023-10-09 22:34:32 +0200

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+var throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(getData, 500));
+form.addEventListener('submit', cleanData)
+
+const data = {
+};
+
+autoComplete();
+function autoComplete() {
+    const storageData = localStorage.getItem("feedback-form-state");
+    const { email, message } = JSON.parse(storageData);
+        form.email.textContent = email;
+        form.message.textContent = message;
+}
+
+function getData(event) {
+    data.email = form.email.value;
+    data.message = form.message.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+};
+
+
+function cleanData(event) { 
+    event.preventDefault();
+    const data = localStorage.getItem("feedback-form-state");
+    console.log(JSON.parse(data));
+    localStorage.removeItem("feedback-form-state")
+    event.currentTarget.reset();
+}
